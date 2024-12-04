36783b488d51384843e460af3b94ae1834901abd
sheikh0775 <47317414+sheikh0775@users.noreply.github.com>
2024-12-04 16:01:39 +0300

    update text

--- src/JWT.php
+++ src/JWT.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of jwt-auth.
  *
- * (c) Sean Tymon <tymon148@gmail.com>
+ * (c) Sean Tymon <@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
